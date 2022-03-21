73e6fed1a21c6b09f5939b14c3c64d432148e806
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-03-21 18:07:58 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+const fullName = 'Anthony Ngo';
+const isCool = true;
+const totalPets = 0;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+
+console.log(typeof fullName);
+console.log(typeof isCool);
+console.log(typeof totalPets);
+
+const unknown = null;
+console.log('value of Unknown:', unknown);
+console.log(typeof unknown);
+
+var never;
+console.log('value of never:', never);
+console.log(typeof never);
