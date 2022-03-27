293d032484017bd66454910b597ed7de34ac38f6
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-03-27 06:26:20 +0000

    Completed array-manipulation-1 exercise.

---
+++ array-manipulation-1/src/last.js
@@ -0,0 +1,6 @@
+/* exported last */
+function last(array) {
+  const lastIndex = array.length - 1;
+
+  return array[lastIndex];
+}
---
+++ array-manipulation-1/src/reverse.js
@@ -0,0 +1,11 @@
+/* exported reverse */
+function reverse(array) {
+  var newArray = [];
+
+  for (var i = 0; i < array.length; i++) {
+    newArray.unshift(array[i]);
+  }
+
+  return newArray;
+
+}
