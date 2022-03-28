de5dbead6d7c8cb038c90399f4b51c0baeff44ca
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-03-28 18:09:33 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+console.log('hello, world');
+
+console.log(document.querySelector('h1'));
+
+console.dir('h1');
+
+console.log(document.querySelector('#explanation'));
+
+console.dir('#explanation');
+
+console.log(document.querySelector('.hint'));
+
+console.log(console.dir('.hint'));
+
+console.log(document.querySelectorAll('p'));
+
+console.log(document.querySelectorAll('.example-link'));
