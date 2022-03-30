c2f2e20edefa1d9ab54b526ec42dc6af273449da
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-03-30 05:38:25 +0000

    updated javascript-loops-practice.

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -5,7 +5,7 @@ function filterOutStrings(values) {
 
   for (var i = 0; i < values.length; i++) {
 
-    if (typeof values[i] === 'number') {
+    if (typeof values[i] !== 'string') {
       noStrings.push(values[i]);
     }
 
