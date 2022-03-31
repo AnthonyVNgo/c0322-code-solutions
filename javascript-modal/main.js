3ac68c7a52cfc2461e8c638dbf8c57dc2aa7deb6
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-03-31 04:29:01 +0000

    javascript-modal updated

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,32 +1,24 @@
 var modal = document.querySelector('.modal');
 
-// console.log(modal);
-
 var popup = document.querySelector('.display-none');
 
-// console.log(popup);
-
-var click = 0;
+var click = false;
 
 function clicker(event) {
-  if (click === 0) {
+  if (click === false) {
     popup.className = 'card';
-    click++;
-  } else if (click !== 0) {
-    popup.className = 'display-none';
-    click--;
+    click = true;
   }
 }
 
 modal.addEventListener('click', clicker);
 
 var noButton = document.querySelector('.no');
-// console.log(noButton);
 
 function close(event) {
-  if (click !== 0) {
+  if (click === true) {
     popup.className = 'display-none';
-    click--;
+    click = false;
   }
 }
 
