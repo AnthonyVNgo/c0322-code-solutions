9e0e943a0ae82e791806e196d9dd2a02707e98b6
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-03-31 04:23:26 +0000

    javascript-light-bulb update

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -3,46 +3,24 @@ var bgOn = document.querySelector('.bg-on');
 var buttonOff;
 var bgOff;
 
-// console.log(buttonOn);
-// console.log(bgOn);
-
-var button = 0;
+var button = false;
 
 function buttonSwitch(event) {
 
-  if (button === 0) {
+  if (button === false) {
     buttonOn.className = 'button-off column-25 height-25 align-self-center border-radius-circle';
     bgOn.className = 'bg-off';
-    button++;
-    // console.log(button);
+    button = true;
 
     buttonOff = document.querySelector('div.button-off');
     bgOff = document.querySelector('.bg-off');
-    // console.log(buttonOff);
-    // console.log(bgOff);
-  } else if (button === 1) {
+
+  } else if (button === true) {
     bgOff.className = 'bg-on';
     buttonOff.className = 'button-on box-shadow column-25 height-25 align-self-center border-radius-circle';
-    button--;
-    // console.log(button);
+    button = false;
+
   }
 }
 
 buttonOn.addEventListener('click', buttonSwitch);
-
-// console.log(buttonOff);
-// console.log(bgOff);
-
-// function buttonSwitchOff(event) {
-//   buttonOff.className = 'button-on column-25 height-25 align-self-center border-radius-circle';
-//   bgOff.className = 'bg-on';
-// }
-
-// if (bgOff.classList.contains('bg-off')) {
-//   bgOff.className = 'bg-on';
-// }
-
-// function buttonSwitchReverse(event) {
-//   buttonOff.className = 'button-on column-25 height-25 align-self-center border-radius-circle';
-//   bgOff.className = 'bg-on';
-// }
