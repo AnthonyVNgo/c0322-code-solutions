600509544a959530c5bbecee3ef017147fad1d83
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-01 07:00:20 +0000

    javascript-typing-tutor WIP

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,30 @@
+
+// var pElement = document.querySelector('.change-me');
+// console.log(pElement.textContent);
+
+// var pElementText = pElement.textContent;
+// console.log(pElementText);
+
+// var pElementTextSplit = pElementText.split('');
+// console.log(pElementTextSplit);
+
+// function matchedKeys(event) {
+//   if () {
+//     // if key down doesn't match pElementTextSplit[i], then change char color to red
+//   } // else if key down matches pElementTextSplit[i], then change char color to green and i++?
+// }
+
+// document.addEventListener('keydown')
+
+// console.log(textIndexing[0]);
+
+// function matchKeys(event) {
+
+//   for (var i = 0; i < textIndexing.length; i++) {
+//     if (keyEvent === textIndexing[i]) {
+//       className = 'red';// change class name for span
+//     }
+//   }
+// }
+
+// document.addEventListener('keydown', matchKeys);
