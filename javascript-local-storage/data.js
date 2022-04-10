dc5f94c0af2b841a4cd3ab31449150b1251057c3
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-10 05:19:31 +0000

    javascript-local-storage update1

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -7,29 +7,9 @@ if (previousTodosJSON !== null) {
   todos = JSON.parse(previousTodosJSON);
 }
 
-// function eventFunction(event) {
-//   var todosJSON = JSON.stringify(todos);
-//   localStorage.setItem('javascript-local-storage', todosJSON);
-// }
-
-// window.addEventListener('beforeunload', eventFunction);
-
-// function removeItem(event) {
-
-// }
-
-// .addEventListener('dblclick')
-
-var listContainer = document.querySelector('#todo-list');
-
-function hideFunction(event) {
-  // console.log(event.target);
-  // console.log(event.target.tagName);
-
-  if (event.target.tagName === 'LABEL' || event.target.tagName === 'INPUT') {
-    var closestAncestor = event.target.closest('.list-group-item');
-    closestAncestor.remove();
-  }
+function eventFunction(event) {
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
 }
 
-listContainer.addEventListener('dblclick', hideFunction);
+window.addEventListener('beforeunload', eventFunction);
