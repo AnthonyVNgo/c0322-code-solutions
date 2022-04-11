ff822eaaf6e85503104d2153bea4e50f0e52b470
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-11 21:47:58 +0000

    string-manipulation-3

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,4 +1,10 @@
-/* exported isAnagram */
+// /* exported isAnagram */
 // function isAnagram(firstString, secondString) {
+//   var firstStringArr = firstString.split('');
+//   var secondStringArr = secondString.split('');
+//   console.log(firstStringArr);
+//   console.log(secondStringArr);
 
+//   var firstStringArrSorted = firstStringArr.sort();
+//   var secondStringArrSorted = secondStringArr.sort();
 // }
