662ea37f38f0343ca2b53e88c379dc357d1910a2
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-11 21:02:50 +0000

    array-manipulation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -7,9 +7,11 @@ function chunk(array, size) {
 
     if (i === 0) {
       mainArr.push(array.slice(i, i + size));
+    } else if (size === 1) {
+      mainArr.push(array.slice(i, i + size));
     } else if (i === size) {
       mainArr.push(array.slice(i, i + size));
-    } else if (size === 1) {
+    } else if (i === size * 2) {
       mainArr.push(array.slice(i, i + size));
     }
   }
@@ -19,3 +21,6 @@ function chunk(array, size) {
 
 // .slice(i, i+size)
 // if (array.length % size === 0) {
+
+// we want the code to create a new array using the elements from the array argument
+// we want the code to create array chunks based on the value of the size argument
--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -4,3 +4,8 @@ function take(array, count) {
   return newArray;
 
 }
+
+// we want our code to return a new array with the elements from the array passed in as an argument, with the length of the array equal to the count, starting from the front.
+// we can produce the result we want by using the slice method and setting the end point for the slice method as the value passed in from the count argument
+// we assign the result of the method being called for the array object to newArray
+// we return newArray
