f00aafb7ad462413d9b4a89788138561fdec23a2
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-11 19:04:32 +0000

    array-manipulation-1 updated

--- array-manipulation-1/src/reverse.js
+++ array-manipulation-1/src/reverse.js
@@ -9,3 +9,9 @@ function reverse(array) {
   return newArray;
 
 }
+
+// we want our code to reverse the array passed in as an argument
+// we first create an array and assign it to a new variable (var newArray)
+// we can then create a for loop to loop through the elements within the array passed in as an argument
+// instead of pushing the elements from the argument, we can unshift the elements. This should place older elements towards the end and newer elements in front
+// finally we can return the newArray
