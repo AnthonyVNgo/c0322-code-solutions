0fedb31c97f5da7a2fe62d10103f052c9022e7fe
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-13 18:37:12 +0000

    javascript-timers update2

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,16 +1,21 @@
 var countdownElement = document.querySelector('.countdown-display');
 var startingNumber = 4;
+var intervalID = setInterval(countdown, 1000);
 
 function countdown() {
   // countdownElement.textContent.replaceWith(countdownElement.textContent--);
-  countdownElement.textContent = startingNumber--;
+  if (startingNumber > 0) {
+    countdownElement.textContent = startingNumber--;
+  } else if (startingNumber === 0) {
+    countdownElement.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(intervalID);
+  }
+
 }
 
-var intervalID = setInterval(countdown, 1000);
+// function atZero() {
+//   countdownElement.textContent = '~Earth Beeeelooowww Us~';
+//   setTimeout(clearInterval(intervalID));
+// }
 
-function atZero() {
-  countdownElement.textContent = '~Earth Beeeelooowww Us~';
-  setTimeout(clearInterval(intervalID));
-}
-
-setTimeout(atZero, 5000);
+// setTimeout(atZero, 5000);
