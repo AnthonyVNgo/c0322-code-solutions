c430b3f2ce588e18b13c572ccfab7a7efb4fc25a
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-14 21:37:58 +0000

    javascript-ajax wip

---
+++ javascript-ajax/users.js
@@ -0,0 +1,21 @@
+var userListElement = document.querySelector('#user-list');
+
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function () {
+  console.log(xhr.status);
+  console.log(xhr.response);
+
+  // review
+  var users = xhr.response;
+  for (var i = 0; i < users.lenght; i++) {
+    var user = users[i];
+    var li = document.createElement('li');
+    li.textContent = user.name;
+    userListElement.appendChild(li);
+  }
+});
+
+xhr.send();
