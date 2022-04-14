9c0722b3055bd3ff6fd1ac7489752f954a749799
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-14 21:41:17 +0000

    javascript-timers update1

--- javascript-timers/timeout.js
+++ javascript-timers/timeout.js
@@ -1,2 +1,8 @@
 var messageElement = document.querySelector('.message');
-messageElement.textContent = 'Hello There';
+// messageElement.textContent = 'Hello There';
+
+function replaceText() {
+  messageElement.textContent = 'Hello There';
+}
+
+setTimeout(replaceText, 2000);
