a8896f4a2134d9c3e06e62e2166b52a1e9ce7773
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-15 17:05:59 +0000

    javascript-ajax wip

--- javascript-ajax/users.js
+++ javascript-ajax/users.js
@@ -1,8 +1,13 @@
 var userListElement = document.querySelector('#user-list');
 
 var xhr = new XMLHttpRequest();
+// xhr represents the X-ml H-ttp R-equest
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+
+// xhr.open() is a method for the XHR object, which is the object that handles browser requests
+// xhr.open() is used to send a request to a server
+// this is part one of two of how AJAX works (request and response)
 xhr.responseType = 'json';
 xhr.addEventListener('load', function () {
   console.log(xhr.status);
