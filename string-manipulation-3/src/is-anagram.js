46cc886ab2db284773943d7500d6df7bfcb744bc
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-04-18 06:55:51 +0000

    string-manipulation-3

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,10 +1,19 @@
-// /* exported isAnagram */
+// // /* exported isAnagram */
 // function isAnagram(firstString, secondString) {
 //   var firstStringArr = firstString.split('');
 //   var secondStringArr = secondString.split('');
-//   console.log(firstStringArr);
-//   console.log(secondStringArr);
 
 //   var firstStringArrSorted = firstStringArr.sort();
 //   var secondStringArrSorted = secondStringArr.sort();
+
+//   var firstStringResults = firstStringArrSorted.filter(removeSpaces);
+//   var secondStringResults = secondStringArrSorted.filter(removeSpaces);
+
+//   if (firstStringResults === secondStringResults) {
+//     return true;
+//   }
+// }
+
+// function removeSpaces(chars) {
+//   return chars !== ' ';
 // }
