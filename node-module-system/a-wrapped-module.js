dc0004af070b0c5e5d3b24137aafcf5a524e3f76
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-05-04 00:14:08 +0000

    node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,15 @@
+console.log(`this is the exports variable ${exports}`);
+console.log('this is the value of exports variable:', exports);
+console.log('this is the type of for exports', typeof exports);
+
+console.log('this is the value of require variable:', require);
+console.log('this is the type of for require:', typeof require);
+
+console.log('this is the value of module variable:', module);
+console.log('this is the type of for module:', typeof module);
+
+console.log('this is the value of __filename variable:', __filename);
+console.log('this is the type of for __filename:', typeof __filename);
+
+console.log('this is the value of __dirname variable:', __dirname);
+console.log('this is the type of for __dirname:', typeof __dirname);
