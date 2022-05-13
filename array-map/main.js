4beb61662b0c3d18e8a96a7cbe89b937ae7373c1
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-05-13 19:44:00 +0000

    array-map completed exercise

---
+++ array-map/main.js
@@ -0,0 +1,17 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const languages = [
+  'Hypertext Markup Language',
+  'Cascading Style Sheets',
+  'ECMAScript',
+  'PHP Hypertext Preprocessor',
+  'Structured Query Language'
+];
+
+const doubled = numbers.map(element => element * 2);
+const prices = numbers.map(element => element.toLocaleString('en-US', { style: 'currency', currency: 'USD' }));
+const upperCased = languages.map(element => element.toUpperCase());
+const firstLetters = languages.map(element => {
+  const splitArr = element.split(' ');
+  return splitArr.map(element => element[0]);
+});
