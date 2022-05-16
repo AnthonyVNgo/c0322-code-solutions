58db307d348266c2996c3ca1a5f3301b5400df41
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-05-16 02:03:11 +0000

    string-manipulation-3 completed exercise

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,45 +1,6 @@
 /* exported isAnagram */
-// function isAnagram(firstString, secondString) {
-//   var firstStringArr = firstString.split('');
-//   var secondStringArr = secondString.split('');
-
-//   var firstStringArrSorted = firstStringArr.sort();
-//   var secondStringArrSorted = secondStringArr.sort();
-
-//   var firstStringResults = firstStringArrSorted.filter(removeSpaces);
-//   var secondStringResults = secondStringArrSorted.filter(removeSpaces);
-
-//   if (firstStringResults === secondStringResults) {
-//     return true;
-//   }
-// }
-
-// function removeSpaces(chars) {
-//   return chars !== ' ';
-// }
-
-function isAnagram(firstString, secondString) {
-  // console.log(removeSpaces(firstString));
-  // console.log(removeSpaces(secondString));
-  // console.log(removeSpaces(firstString).length);
-  // console.log(removeSpaces(secondString).length);
-  var x = sortString(removeSpaces(firstString)).join();
-  var y = sortString(removeSpaces(secondString)).join();
-
-  if (x === y) {
-    return true;
-  } else {
-    return false;
-  }
-
-  // if (removeSpaces(firstString).length !== removeSpaces(secondString).length) {
-  //   return false;
-  // }
-
-}
-
 function removeSpaces(string) {
-  var stringWithoutSpaces = string.replace(' ', '');
+  var stringWithoutSpaces = string.replace(/ /g, '');
   return stringWithoutSpaces;
 }
 
@@ -49,5 +10,12 @@ function sortString(string) {
   return sortedArrayIntoString;
 }
 
-// if (removeSpaces(firstString).length !== removeSpaces(secondString.length)) {
-//   return false;
+function isAnagram(firstString, secondString) {
+  var x = sortString(removeSpaces(firstString)).join();
+  var y = sortString(removeSpaces(secondString)).join();
+  if (x === y) {
+    return true;
+  } else {
+    return false;
+  }
+}
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,93 +1,58 @@
 /* exported titleCase */
-// function titleCase(title) {
-//   // return capitalizeWords(title);
-//   // console.log(capitalizeWords(title));
-//   // capitalizeWords(title).replace('And', 'and').replace('Or', 'or').replace();
 
-//   var lowerCaseInput = title.toLowerCase();
-//   var inputLowerCasedSplit = lowerCaseInput.split(' ');
+function capitalize(word) {
+  var firstLetter = word[0];
+  var firstLetterUpperCased = firstLetter.toUpperCase();
+  var rest = word.slice(1);
+  var lowercaseRest = rest.toLowerCase();
+  return firstLetterUpperCased + lowercaseRest;
+}
 
-//   capitalizeWord(inputLowerCasedSplit[0]);
-//   // console.log(capitalizeWord(inputLowerCasedSplit[0]));
+function allcaps(word) {
+  return word.toUpperCase();
+}
 
-//   console.log(capitalizeWords(title));
-// }
+function notthese(string) {
+  if (string === 'and' || string === 'or' || string === 'nor' || string === 'but' || string === 'a' || string === 'an' || string === 'the' || string === 'as' || string === 'at' || string === 'by' || string === 'for' || string === 'in' || string === 'of' || string === 'on' || string === 'per' || string === 'to') {
+    return true;
+  } return false;
+}
 
-// /*
+function dashHandler(string) {
+  const splitString = string.split('-');
+  const dashOutput = [];
+  for (var j = 0; j < splitString.length; j++) {
+    dashOutput.push(capitalize(splitString[j]));
+  }
+  return dashOutput.join('-');
+}
 
-// capitalize character at first index
-// if word/string = and, or, nor, but, a, an, the, as, at, by, for, in, of, on, per, to, then they will be lowercase
-// but if the previous list of words are first, then they will be capitalized
-
-// 1. split the string passed in as an argument with the separator being ' ', creating a new array containing the values of the string separated by the space
-// 2. assign the new array to a variable so we can manipulate it
-// 3. capitalize the first letter of the first array element regardless of what word it is
-// 4. slice first letter
-// 5. capitalize first letter
-// 6. slice chars after first letter
-// 7. lowercase remaining letters
-// 8. concatenate
-
-// . create a for loop initialized at 1 (the second array element), set the condition expression to the
-
-// */
-
-// function capitalizeWord(word) {
-//   var firstLetter = word.slice(0, 1).toUpperCase();
-//   var rest = word.slice(1).toLowerCase();
-//   var newString = firstLetter + rest;
-
-//   if (newString === 'Javascript') {
-//     return 'JavaScript';
-//   } else {
-//     return newString;
-//   }
-// }
-
-// function capitalizeWords(input) {
-//   for (var i = 1; i < input.length; i++) {
-
-//     if (input !== 'and' || input !== 'or' || input !== 'nor' ||
-//       input !== 'but' || input !== 'a' || input !== 'an' ||
-//       input !== 'the' || input !== 'as' || input !== 'at' ||
-//       input !== 'by' || input !== 'for' || input !== 'in' ||
-//       input !== 'of' || input !== 'on' || input !== 'per' ||
-//       input !== 'to' || input !== 'then') {
-
-//       var firstChar = input.slice(0, 1).toUpperCase();
-//       var remainingChar = input.slice(1).toLowerCase();
-
-//       newWord = firstChar + remainingChar;
-//       newString += newWord + ' ';
-//     }
-//   }
-//   return newString.slice(0, newString.length - 1);
-// }
-
-// // function lowerCaseMinorWords(input) {
-// //   if (input !== 'and' || input !== 'or' || input !== 'nor' ||
-// //     input !== 'but' || input !== 'a' || input !== 'an' ||
-// //     input !== 'the' || input !== 'as' || input !== 'at' ||
-// //     input !== 'by' || input !== 'for' || input !== 'in' ||
-// //     input !== 'of' || input !== 'on' || input !== 'per' ||
-// //     input !== 'to' || input !== 'then') {
-// //     // console.log('detected input value: ' + input);
-// //     input.toLowerCase();
-// //   }
-// // }
-
-// // function capitalizeWords(string) {
-// //   var splitString = string.split(' ');
-// //   var newWord = '';
-// //   var newString = '';
-
-// //   for (var i = 0; i < splitString.length; i++) {
-// //     var firstChar = splitString[i].slice(0, 1).toUpperCase();
-// //     var remainingChar = splitString[i].slice(1).toLowerCase();
-
-// //     newWord = firstChar + remainingChar;
-// //     newString += newWord + ' ';
-// //   }
-
-// //   return newString.slice(0, newString.length - 1);
-// // }
+function titleCase(title) {
+  const lowerCased = title.toLowerCase();
+  const splitTitleArr = lowerCased.split(' ');
+  const outputArr = [];
+  for (var i = 0; i < splitTitleArr.length; i++) {
+    if (splitTitleArr[i] === 'javascript:') {
+      outputArr.push('JavaScript:');
+      outputArr.push(capitalize(splitTitleArr[i + 1]));
+      i++;
+    } else if (splitTitleArr[i] === 'javascript') {
+      outputArr.push('JavaScript');
+    } else if (splitTitleArr[i] === 'api') {
+      outputArr.push(allcaps(splitTitleArr[i]));
+    } else if (i === 0) {
+      outputArr.push(capitalize(splitTitleArr[i]));
+    } else if (splitTitleArr[i].includes(':')) {
+      outputArr.push(capitalize(splitTitleArr[i]));
+      outputArr.push(capitalize(splitTitleArr[i + 1]));
+      i++;
+    } else if (splitTitleArr[i].includes('-')) {
+      outputArr.push(dashHandler(splitTitleArr[i]));
+    } else if (notthese(splitTitleArr[i]) === false) {
+      outputArr.push(capitalize(splitTitleArr[i]));
+    } else if (notthese(splitTitleArr[i]) === true) {
+      outputArr.push(splitTitleArr[i]);
+    }
+  }
+  return outputArr.join(' ');
+}
