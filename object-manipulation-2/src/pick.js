3995e275fb4d025720fff5d935a029fc19d4f2a8
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-05-17 07:37:47 +0000

    object-manipulation-2 wip

---
+++ object-manipulation-2/src/pick.js
@@ -0,0 +1,12 @@
+// /* exported pick */
+// function pick(source, keys) {
+//   const outputObject = {};
+//   for (var i = 0; i < keys.length; i++) {
+//     const property = keys[i];
+//     outputObject.property = source.property;
+//   }
+//   return outputObject;
+// }
+
+// // source = reference object
+// // keys = an array of keys we want to pick from the source = reference object
