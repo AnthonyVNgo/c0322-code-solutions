4aea3855f064fde5bc1bb6541889a446ece7252b
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-05-24 18:24:57 +0000

    express-static completed exercise

---
+++ express-static/index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
+
+const aVariable = path.join(__dirname, 'public');
+console.log(__dirname);
+console.log(aVariable);
+
+const anotherVariable = express.static('public');
+console.log(anotherVariable);
+app.use(anotherVariable);
