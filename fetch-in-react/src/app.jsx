68b56cfb86766c192e6c7dbee8697085f305d8f7
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-05-26 00:49:58 +0000

    fetch-in-react completed exercise

--- fetch-in-react/src/app.jsx
+++ fetch-in-react/src/app.jsx
@@ -12,11 +12,14 @@ class App extends React.Component {
 
   componentDidMount() {
     /* your code here */
-    const newArr = [];
-    const responsePromise = fetch('https://jsonplaceholder.typicode.com/users');
-    const dataPromise = responsePromise.then(response => response.json());
-    dataPromise.then(data => newArr.push(data));
-    this.setState({ isLoading: !this.state.isLoading, users: newArr });
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ isLoading: !this.state.isLoading, users: data });
+      });
+
   }
 
   render() {
