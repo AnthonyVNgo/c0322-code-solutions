d5bbe81071b90d4d2664ef8c9ba2aac64b698c4f
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-06-15 06:07:45 +0000

    data-structure-stacks wip

---
+++ data-structures-stacks/count-values.js
@@ -0,0 +1,13 @@
+/* exported countValues */
+
+function countValues(stack) {
+  // console.log(stack.print());
+  // console.log(Object.keys(stack).length);
+  // return Object.keys(stack).length;
+
+  // let count = 0;
+  // while (typeof stack.pop() !== 'undefined') {
+  //   count++;
+  // }
+  // return count;
+}
---
+++ data-structures-stacks/is-empty.js
@@ -0,0 +1,12 @@
+/* exported isEmpty */
+
+function isEmpty(stack) {
+  // if (stack.peek() === undefined) {
+  //   return true;
+  // }
+  // return false;
+
+  // return stack.peek() === undefined ? true : false
+  // why does line 11 pass?
+  return stack.peek() === undefined;
+}
