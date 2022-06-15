6a58e6dc4b9c68368c8e6e6011acd89cac85caf1
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-06-15 22:15:55 +0000

    data-structures-queues completed exercise

--- data-structures-queues/take-smaller.js
+++ data-structures-queues/take-smaller.js
@@ -1,5 +1,26 @@
 /* exported takeSmaller */
 
 function takeSmaller(queue) {
-  // console.log(queue.print());
+
+  if (queue.peek() === undefined) {
+    return undefined;
+  }
+
+  const first = queue.dequeue();
+  const second = queue.dequeue();
+
+  if (second === undefined) {
+    return first;
+  }
+
+  if (first < second) {
+    queue.enqueue(second);
+    return first;
+  } else if (first > second) {
+    queue.enqueue(first);
+    return second;
+  } else if (first === second) {
+    queue.enqueue(first);
+    return first;
+  }
 }
