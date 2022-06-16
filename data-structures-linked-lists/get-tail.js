830afecbe058e7918deb232f5e55325885457ec6
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-06-16 22:45:05 +0000

    data-structures-linked-lists wip

--- data-structures-linked-lists/get-tail.js
+++ data-structures-linked-lists/get-tail.js
@@ -1,17 +1,19 @@
 /* exported getTail */
 
 function getTail(list) {
-  if (list.next === null) {
-    // console.log(list.data);
-    return list.data;
-  }
+  // if (list.next === null) {
+  //   return list.data;
+  // }
 
-  let nextLl = list.next;
-  while (nextLl.next !== null) {
-    nextLl = nextLl.next;
-    if (nextLl.next === null) {
-      // console.log(nextLl.data);
-      return nextLl.data;
-    }
-  }
+  // let nextLl = list.next;
+  // while (nextLl !== null) {
+  //   nextLl = nextLl.next;
+  // }
+  // if (nextLl === null) {
+  //   // console.log(nextLl);
+  //   console.log(nextLl.data);
+  //   // return nextLl;
+  // }
+  while (list.next) list = list.next;
+  return list.data;
 }
--- data-structures-linked-lists/remove-tail.js
+++ data-structures-linked-lists/remove-tail.js
@@ -1,5 +1,14 @@
 /* exported removeTail */
 
 function removeTail(list) {
+  // if (list.next === null) return;
+  // let prev = list;
+  // let next = list.next;
+  // while (next.next !== null) {
+  //   prev = next;
+  //   next = next.next;
 
+  // }
+  // prev.next = null;
+  // return list;
 }
