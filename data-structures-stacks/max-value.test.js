20e12d34add6a7a1b5b35fbed16e0e5e857367fb
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-06-17 18:51:47 +0000

    data-structures-stacks completed exercise

--- data-structures-stacks/max-value.test.js
+++ data-structures-stacks/max-value.test.js
@@ -29,9 +29,3 @@ describe('maxValue(stack)', () => {
   });
 
 });
-
-// let max = -Infinity;
-// while (typeof stack.peek() !== 'undefined') {
-//   max = Math.max(max, stack.pop());
-// }
-// return max;
