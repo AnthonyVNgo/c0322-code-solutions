66539aa5ae589661c7537abe9315be88beb40a07
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-06-17 08:19:39 +0000

    data-structures-linked-lists wip

--- data-structures-linked-lists/remove-tail.js
+++ data-structures-linked-lists/remove-tail.js
@@ -8,18 +8,16 @@ function removeTail(list) {
   // this takes care of the list if it only contains ONE node
 
   let prev = list;
-  // console.log(prev);
   let next = list.next;
-  // console.log(next);
-  // console.log(next.next);
   while (next.next !== null) {
     prev = next;
     next = next.next;
   }
-  // console.log(prev);
-  // console.log(next);
-  // console.log(prev.next);
+
+  // if next.next === null, then prev.next = null
+  // return list returns 1 node if there's only 2 nodes
   prev.next = null;
+
   // prev.next ≠ prev.list.next because we're referencing the property next with the dot, not the variable
   // null is assigned to prev.next, which means the prev (head node) no longer has a second node following after it
   // if prev = list, and list.next returns null, then return list returns
@@ -31,5 +29,10 @@ function removeTail(list) {
 // line 11 has a condition statement to check if next.next doesn't return null
 // the condition statement checks to see if there's another node following the second node
 // there MUST be a second node to check because of the guard placed on line 4 which checks if the list has another node
-// if the list has at least a second node, then it is checked again on line 11
-// if the list doesn't have a third node, then the while loop is skipped, and
+// on line 12, the condition statement for the while loop checks if the return of next.next (the third node) is not strictly equal to null
+// the condition statement begins at the next.next (the second node) because we want to end before we reach the tail, where prev.next returns null
+// we want to access the second to last node and set its next to null to remove the last node
+
+// returns nothing if there's 1 node
+// returns 1 node if there's 2 nodes
+// returns whatever amount after 2+ nodes
