be3779355e571d696ca0095aff88f2fc133f7c61
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-06-29 07:57:34 +0000

    array-manipulation-3 flatten wip

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,17 @@
 /* exported flatten */
+function flatten(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i]) === false) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+
+// return a NEW array // newArray
+// test whether a value is an array with Array.isArray
+// test whether each element in the array is an array with Array.isArray
+
+// if the element in the array, array[i], is not an array, push it into the new array newArray
+// if the elemnt in the array, array[i], is an array, then loop through the array and push the elements into the new array newArray
