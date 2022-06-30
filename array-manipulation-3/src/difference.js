68ad602f26a07bb9e94189aa42e08d8a165e11fc
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-06-30 02:45:36 +0000

    array-manipulation-3 difference completed

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,4 +1,25 @@
 /* exported difference */
 function difference(first, second) {
+  const outputArr = [];
 
+  for (let i = 0; i < first.length; i++) {
+    if (!second.includes(first[i])) {
+      outputArr.push(first[i]);
+    }
+  }
+
+  for (let j = 0; j < second.length; j++) {
+    if (!first.includes(second[j])) {
+      outputArr.push(second[j]);
+    }
+  }
+  return outputArr;
 }
+
+// A new Array containing the symmetric difference between first and second.
+// That is, elements are included if they are found in one Array,
+// but not the other.
+// The result values are unqiue and
+// their order is determined by the order they occur.
+
+// can i use set again to get unique values?
