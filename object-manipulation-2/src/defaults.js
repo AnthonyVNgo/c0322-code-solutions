4a04eeeb181c296302e32019e9ffa1a87cafbf6e
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-06-30 20:01:07 +0000

    object-manipulation-2 completed exercise

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,17 @@
 /* exported defaults */
+function defaults(target, source) {
+  for (const keys in source) {
+    if (!Object.keys(target).includes(keys)) {
+      target[keys] = source[keys];
+    }
+  }
+}
+
+// if target doesn't include properties from source, then add the source properties into the target object
+// add property with bracket notation
+// // console.log(`${keys} not included`);
+
+// line 3. use a for in loop/statement to loop through the keys in the source SVGForeignObjectElement
+// line 4. use a condition statement, Object.keys(), and includes(), to check if the target object's keys (extracted with the Object.keys method and returns an array of the object's keys)
+// does not include the keys from the source object
+// line 5. if the target object does not include the keys from the source object, then use bracket notation to add the key [keys] to the target object and assign it the value of the return of the source object's key [key]
--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,5 +1,4 @@
 /* exported invert */
-
 function invert(source) {
   const newObject = {};
   for (const keys in source) {
@@ -7,3 +6,9 @@ function invert(source) {
   }
   return newObject;
 }
+
+// line 3. define an empty output object named newObject
+// line 4. use a for in loop/statement to loop through the keys in the source object passed as an argument
+// line 5. use bracket notation to add the return value of source[keys] (the source object's key/property's value) to the newObject as one of its keys/properties
+// line 5. assign keys (the source object's keys) as the value for the newObject's newly added key/property (the newObject's newly added key/property is the source object's key value)
+// line 7. return newObject
