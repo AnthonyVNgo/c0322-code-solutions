3726a8805967bea96a8518d2c397d5e5453043b7
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-06-30 04:41:21 +0000

    array-manipulation-3 completed!

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,20 @@
 /* exported intersection */
+function intersection(first, second) {
+  const outputArr = [];
+
+  for (let i = 0; i < first.length; i++) {
+    if (second.includes(first[i])) {
+      outputArr.push(first[i]);
+    }
+  }
+
+  for (let j = 0; j < second.length; j++) {
+    if (first.includes(second[j])) {
+      outputArr.push(second[j]);
+    }
+  }
+
+  const uniqueSet = new Set(outputArr);
+  const backToArr = [...uniqueSet];
+  return backToArr;
+}
