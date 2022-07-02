1a2e3e0b3365b7812111fa20524b8f2fbb38f05e
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-07-02 18:12:07 +0000

    object-manipulation-3 pseudocode

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -33,67 +33,31 @@ function deal() {
   });
   const playerValues = players.map(player => player.value);
   const highestValue = Math.max(...playerValues);
-  console.log(players);
   return `player ${playerValues.indexOf(highestValue) + 1} wins, with a value of: ${players[playerValues.indexOf(highestValue)].value}`;
 }
 deal();
 
-// function calculateValue(currentElement) {
-// let value = 0;
-// for (let i = 0; i < currentElement.hand.length; i++) {
-//   if (currentElement.hand[i].rank === 'A') {
-//     value += 11;
-//   } else if (currentElement.hand[i].rank === 'J' || currentElement.hand[i].rank === 'Q' || currentElement.hand[i].rank === 'K') {
-//     value += 10;
-//   } else if (currentElement.hand[i].rank !== 'A' || currentElement.hand[i].rank !== 'J' || currentElement.hand[i].rank !== 'Q' || currentElement.hand[i].rank !== 'K') {
-//     value += currentElement.hand[i].rank;
-//   }
-// }
-// currentElement.value = value;
-// }
+// define a variable named suit and assign it an array containing 4 string elements corresponding to each suit
+// define a variable named rank and assign it an array containing 5 string elements corresponding to each face rank + ace, and 9 number elements corresponding to each number rank
+// the combination of values from both of these arrays will be used to construct 52 individual card objects.
+// the individual card objects will be used to form a deck
 
-// A = 11
-// J,Q,K = 10
+// define a variable named players and assign it an array containing 4 empty objects
+// the assignment of properties can be done later
 
-// if i === A, then value += 11
-// if i === j,q,k then value += 10
-// if i !== a,j,q,k, then value += i
-// function dealToPlayers() {
-//   const dealt = (_.sampleSize(createDeck(), 8));
-//   dealt.splice(0, 2);
-// }
-
-// 05698
-
-//  high to low spade hearts diamond clubs
-
-// const dealt = (_.sampleSize(createDeck(), 8));
-
-// _.sampleSize returns an array
-// .splice returns an array as well
-// currentElement.hand.push(_.sampleSize(deck, 8).splice(0, 2));
-// currentElement.hand.push(_.shuffle(deck).splice(0, 2));
-// console.log(currentElement.hand[0]);
-
-// function calculateValue(currentElement) {
-//   let value = 0;
-//   for (let i = 0; i < currentElement.hand.length; i++) {
-//     if (currentElement.hand[0][i].rank === 'A') {
-//       value += 11;
-//     } else if (currentElement.hand[0][i].rank === 'J' || currentElement.hand[0][i].rank === 'Q' || currentElement.hand[0][i].rank === 'K') {
-//       value += 10;
-//     } else if (currentElement.hand[0][i].rank !== 'A' || currentElement.hand[0][i].rank !== 'J' || currentElement.hand[0][i].rank !== 'Q' || currentElement.hand[0][i].rank !== 'K') {
-//       value += i;
-//     }
-//   }
-//   return value;
-// }
-
-// const Player = class {
-//   constructor(name, hand, value) {
-//     this.name = name;
-//     this.hand = hand;
-//     this.value = value;
-//   }
-// };
-// const players = [new Player('p1', []), new Player('p2', []), new Player('p3', []), new Player('p4', [])];
+// define a function createDeck
+// within the createDeck function's code block:
+// define a variable named deck and assign it an empty array, this will be used to store the value of the cards that will be created in the next few lines
+// use a for loop to iterate through the length of the suit array (4 iterations)
+// within the for loop's code block, use another for loop to iterate through the length of the rank array (13 iterations)
+// within the nest for loop's code block, define a variable named card and assign it an object containing the property suit with the value of suit[i] and rank with the value of rank[j]
+// push the variable card / card object into the deck array
+// the for loop's operation starts and creates each set of suits first,
+// meaning that i does not increment until the nested for loop completes its operation,
+// meaning that the deck pushes in card objecs with the suit property value of suit[0] and rank property values of rank[0-13],
+// because the nested for loop's operation runs while the condition has not been met (j < rank.length)
+// once the nested for loop's condition is met,
+// THEN the parent for loop's code block/statement is completed
+// THEN the final expression runs and i increments
+// use lodash _.shuffle(deck) to return an array of shuffled elements
+// return the value of _.shuffled(deck)
