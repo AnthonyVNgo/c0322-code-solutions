6bb44f539c407793e12ce94a382fcdff220a3a19
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-07-02 19:19:00 +0000

    array-manipulation-3 updated zip function code

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,29 +1,14 @@
 /* exported zip */
 function zip(first, second) {
   const mainArr = [];
-  if (first.length > second.length) {
-    while (first.length !== second.length) {
-      first.pop();
-    }
-  }
-  while (second.length !== first.length) {
-    second.pop();
-  }
-
-  for (let i = 0; i < first.length; i++) {
+  const length = Math.min(first.length, second.length);
+  for (let i = 0; i < length; i++) {
     mainArr.push([first[i], second[i]]);
   }
   return mainArr;
 }
 
-// line 3. declare a variable for the output array, named mainArr for the function to return at the end of its operation
-// line 4-8
-// include a guard to make the first array the same length as the second array
-
-// line 9-11
-// include a guard to make the second array the same length as the first array
-
-// line 13-15
-// use a for loop and within its codeblock, push into the mainArr an array that contains the values of the first and second array at index[i]
-
-// line 16. return the output array mainArr
+// line 4. use Math.min to return the lowest value passed in as its arguments
+// line 5. use a for loop to iterate through the length of the length variable
+// line 6. push into the mainArr the elements from the first and second array at index [i]
+// line 8. return the mainArr variable/array
