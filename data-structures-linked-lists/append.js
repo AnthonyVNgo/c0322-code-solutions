6d0c432c75846b4e1582bdd0956b228af7e3d580
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-07-06 22:27:59 +0000

    data-structures-linked-lists updated

--- data-structures-linked-lists/append.js
+++ data-structures-linked-lists/append.js
@@ -5,7 +5,6 @@ function append(list, value) {
   while (list.next) list = list.next;
   list.next = new LinkedList(value);
 }
-
 // the same logic from get-getTail.js applies to this exercise, except for the creation of a new LinkedList()
 // once the tail has been reached and assigned to the variable list, a new LinkedList is assigned to the list.next property
 // the new LinkedList is the next node in the list
