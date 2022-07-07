4f74b7ec093d0e09566d7a9f19b702fb66e75b4f
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-07-07 20:56:49 +0000

    react-accordion-component wip

---
+++ react-accordion-component/src/accordion.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Accordion extends React.Component {
+  constructor() {
+    super();
+    this.state = null;
+  }
+
+  render() {
+    return (
+      <p>fuck you</p>
+    );
+  }
+}
