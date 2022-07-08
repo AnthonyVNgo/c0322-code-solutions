a6b095046c03ec1073a28346efad65a8fa6fb0a1
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-07-08 05:45:41 +0000

    react-accordion-component some css to help with visualization

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,19 +4,26 @@ export default class Accordion extends React.Component {
   constructor() {
     super();
     this.state = null;
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    // console.log('bbang');
   }
 
   render() {
     return (
-      <ul>
-        {
-          this.props.topics.map((topic, index) => {
-            return (
-            <AccordionItem key={index} header={topic.header} details={topic.details}/>
-            );
-          })
-        }
-      </ul>
+      <div className="container">
+        <ul>
+          {
+            this.props.topics.map((topic, index) => {
+              return (
+                <AccordionItem key={index} header={topic.header} details={topic.details} clicked={this.handleClick} />
+              );
+            })
+          }
+        </ul>
+      </div>
     );
   }
 }
@@ -24,8 +31,8 @@ export default class Accordion extends React.Component {
 function AccordionItem(props) {
   return (
   <li>
-    <button>
-      <h1>{props.header}</h1>
+    <button onClick={props.clicked}>
+      <h3>{props.header}</h3>
     </button>
     <AccordionDetails details={props.details}/>
   </li>
@@ -34,6 +41,6 @@ function AccordionItem(props) {
 
 function AccordionDetails(props) {
   return (
-    <p>{props.details}</p>
+    <p className='not-hidden'>{props.details}</p>
   );
 }
