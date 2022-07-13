92f77c60021cc0e6e81a282388d0ae2cc424bb6a
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-07-13 19:10:52 +0000

    react-carousel-component onClick function added along with carousel click logic

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -3,23 +3,43 @@ import React from 'react';
 export default class Carousel extends React.Component {
   constructor() {
     super();
-    this.state = null;
+    this.state = {
+      listLength: null,
+      carouselImageId: 0
+    };
+    this.incrementClick = this.incrementClick.bind(this);
+    this.decrementClick = this.decrementClick.bind(this);
   }
 
   componentDidMount() {
     this.setState({ listLength: this.props.urls.length });
   }
 
-  render() {
-    const carouselImageId = 0;
+  incrementClick(event) {
+    if (this.state.carouselImageId === this.state.listLength) {
+      this.setState({ carouselImageId: 0 });
+    } else if (this.state.listLength !== this.state.carouselImageId) {
+      this.setState({ carouselImageId: this.state.carouselImageId + 1 });
+    }
+  }
 
+  decrementClick(event) {
+    if (this.state.carouselImageId === 0) {
+      this.setState({ carouselImageId: this.state.listLength });
+    } else if (this.state.carouselImageId !== 0) {
+      this.setState({ carouselImageId: this.state.carouselImageId - 1 });
+    }
+  }
+
+  render() {
+    const carouselImageId = this.state.carouselImageId;
     return (
       <div>
         <p>{carouselImageId}</p>
-        <button>
+        <button onClick={this.decrementClick}>
           left
         </button>
-        <button>
+        <button onClick={this.incrementClick}>
           right
         </button>
       </div>
