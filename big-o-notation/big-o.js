5476b2fd7bf4bf8c5ac60f05f05571d7505ff75f
Anthony Ngo <Anthony.v.ngo@gmail.com>
2022-07-16 00:21:32 +0000

    big-o-notation wip commentary

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -1,17 +1,17 @@
 /* exported uniqueQuadratic, uniqueLinear */
 
 function uniqueLinear(words) {
-  const seen = {};                  // _ * _ = O(?)
-  const unique = [];                // _ * _ = O(?)
+  const seen = {};                  // 1 * 1 = O(1) declaring / assigning a variable = 1 simple operation
+  const unique = [];                // 1 * 1 = O(1) declaring / assigning a variable = 1 simple operation
   for (
-    let i = 0;                      // _ * _ = O(?)
-    i < words.length;               // _ * _ = O(?)
-    i++                             // _ * _ = O(?)
+    let i = 0;                      // 1 * 1 = O(?) declaring / assigning a variable = 1 simple operation
+    i < words.length;               // 2 * n = O(?) single-step expression + object property access = 2 simple operations
+    i++                             // 1 * n = O(?) single-step operation = 1 simple operation (why is this considered 2 simple operations in the example if i++ is equal to i + 1, which would be a single-step expression?)
   ) {
-    const word = words[i];          // _ * _ = O(?)
-    if (!seen[word]) {              // _ * _ = O(?)
+    const word = words[i];          // 2 * n = O(?) declaring / assigning a variable + array index access = 2 simple operations
+    if (!seen[word]) {              // 3 * n = O(?) truth test + single-step operation (bang logical operator) + array index access = 3 simple operations
       seen[word] = true;
-      unique[unique.length] = word; // _ * _ = O(?)
+      unique[unique.length] = word; // 3 * _ = O(?) array index access + object property access + assigning a variable = 3 simple operations
     }
   }
   return unique;
